99136afd5c76b596b09f6ed6e057014d29967d06
lubabalo <lubabalobuwa64@gmail.com>
2019-05-31 17:10:11 +0200

    Test program files added

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,8 +2,44 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+        public string Prefix(string input)
+        {
+            if (input == null){
+                return null;
+            }
+            if (input == String.Empty){
+                return "0,0:";
+            }
+            int counter =0;
+            string output ="";
+            string[] words = input.Split();
+
+            foreach(String s in words){
+                if(s==""){
+
+                }else{
+                    counter++;
+                }
+            }
+            /*
+            for (int i=0; i<words.Length;i++){
+                if(words[i]==""){
+                    words.RemoveAt(i);
+                }
+                else{
+                    counter++;
+                }
+            }
+             */
+
+            int lngth = input.Length;
+            int NumberOfWords = counter;
+            output = $"{lngth},{NumberOfWords}:{input}";
+            return output;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
